8bb04289b8f0d89a29f07c10c233a0c3370b83f9
Devin1996 <devinchandula@gmail.com>
2021-10-20 03:22:22 +0530

    Added Components for Admin

--- src/App.js
+++ src/App.js
@@ -1,77 +1,31 @@
 //import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Navs';
+import Menu from './components/Menu';
+import Students from './admin/students';
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-          <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"/>
-  <ul class="navbar-nav px-3">
-    <li class="nav-item text-nowrap">
-      <a class="nav-link" href="#">Sign out</a>
-    </li>
-  </ul>
-</nav>
 
-<div class="container-fluid">
-  <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="sidebar-sticky pt-3">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">
-              <span data-feather="home"></span>
-              Dashboard 
-            </a>
-          </li>
-        </ul>
+      <Nav />
 
+      <div class="container-fluid">
+        <div class="row">
 
+          <Menu />
+          <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+
+          <BrowserRouter>
+          <Route path='/admin/students' component={Students}/>
+          </BrowserRouter>
+
+          </main>
+        </div>
       </div>
-    </nav>
-
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
 
 
-      <h2>Section title</h2>
-      <div class="table-responsive">
-        <table class="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Header</th>
-              <th>Header</th>
-              <th>Header</th>
-              <th>Header</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1,001</td>
-              <td>Lorem</td>
-              <td>ipsum</td>
-              <td>dolor</td>
-              <td>sit</td>
-            </tr>
-            <tr>
-              <td>1,002</td>
-              <td>amet</td>
-              <td>consectetur</td>
-              <td>adipiscing</td>
-              <td>elit</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </main>
-  </div>
-  </div>
-
- 
     </div>
   );
 }
